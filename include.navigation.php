3c7304565af36b6ddaecaed28b785ea5e93228b4
Sean Blankenship <sean.blankenship@gmail.com>
2011-12-22 12:35:45 -0500

    refactoring and comments

--- include.navigation.php
+++ include.navigation.php
@@ -6,14 +6,14 @@ $mainnav = array(
     'Nav w/ Dropdown'   => array(
         '#',
         'DropOne'		=> '#',
-        'DropTwo'		=> '#'
+        'DropTwo'		=> '#',
     ),
     'Contact'           => 'contact.php'
 );
 
 
 // do not edit below this line
-function writeNavigation($array, $writeSubNav="", $domain="", $cut_first="") { 
+function writeNavigation($array, $writeSubNav="", $domain="", $cut_first="") {
 
     $domain .= ($domain=="" ? "" : "/"); 
     
@@ -21,46 +21,64 @@ function writeNavigation($array, $writeSubNav="", $domain="", $cut_first="") {
         $array = array_slice($array, 1);
     }
     
-    $i=0;
-    $len=count($array);
-    $first=' class="first"';
-    $last=' class="last"';
+    $i = 1;
+    $len = count($array);
+    $first = ' class="first"';
+    $last = ' class="last"';
     
+    // loop through the array
     foreach ($array as $key => $value){
-    	if ($writeSubNav=="1"){
+
+        // if dropdowns are turned on 
+        if ($writeSubNav=="1"){
+
+            // if the value is an array (if there is a dropdown)
         	if (is_array($value)){
-        		$link = $value[0];
-                echo '<li';
-                if ($i==0){echo $first;} elseif ($i==$len-1){echo $last;}
-                echo '><a href="'.$domain.$link.'">'.$key.'</a><ul>';
-    			foreach (array_slice($value, 1) as $key2 => $value2){
+                $link = $value[0];
+
+                // add in a class of first or last if it is the first or last element in the array
+                echo '<li'. ($i==1 ? $first : ($i==$len ? $last : '')) .'><a href="'.$domain.$link.'">'.$key.'</a><ul>';
+                
+                // loop through the dropdown array
+                foreach (array_slice($value, 1) as $key2 => $value2){
+
+                    // is there a secondary dropdown array
     				if(is_array($value2)){
     					$link2 = $value2[0];
-    					echo '<li><a href="'.$link2.'">'.$key2.'</a><ul>';
+                        echo '<li><a href="'.$link2.'">'.$key2.'</a><ul>';
+
+                        // loop through the secondary dropdown
     					foreach (array_slice($value2, 1) as $key3 => $value3){
     						echo '<li><a href="'.$value3.'">'.$key3.'</a></li>';
     					}
-    					echo '</ul></li>';
+                        echo '</ul></li>';
+
+                    // write the dropdown as normal
     			    } else {
     		    		echo '<li><a href="'.$value2.'">'.$key2.'</a></li>';
     	    		}
            		}
                 echo '</ul></li>';
                 $i++;
+
+            // if the value is not an array
     		} else {
-        		echo '<li';
-                if ($i==0){echo $first;} elseif ($i==$len-1){echo $last;}
-                echo '><a href="'.$domain.$value.'">'.$key.'</a></li>';
+        		echo '<li'. ($i==1 ? $first : ($i==$len ? $last : '')) .'><a href="'.$domain.$value.'">'.$key.'</a></li>';  
                 $i++; 
-    		}
-    	} else {
+            }
+
+        // if dropdowns are turned off
+        } else {
+
+            // if dropdowns are turned off but there is still an array, ignore the array
     		if(is_array($value)){
-    			$link = $value[0];
-    			echo '<li><a href="'.$domain.$link.'">'.$key.'</a></li>';
-       		} else {
-               	echo '<li';
-                if ($i==0){echo $first;} elseif ($i==$len-2){echo $last;}
-                echo '><a href="'.$domain.$value.'">'.$key.'</a></li>';
+                $link = $value[0];
+                echo '<li'. ($i==1 ? $first : ($i==$len ? $last : '')) .'><a href="'.$domain.$link.'">'.$key.'</a></li>';
+                $i++;
+
+            // business as usual
+            } else {
+               	echo '<li'. ($i==1 ? $first : ($i==$len ? $last : '')) .'><a href="'.$domain.$value.'">'.$key.'</a></li>';
                 $i++; 
     		}
     	}
@@ -70,17 +88,13 @@ function writeNavigation($array, $writeSubNav="", $domain="", $cut_first="") {
 $open_nav = '<ul id="navlist" class="clearfix">';
 $close_nav = '</ul>';
 
-if($pageName=="404b"){
+if ($pageName=="404b") {
 	echo $open_nav;
-	writeNavigation($mainnav, "0", $myDomain);
+	writeNavigation($mainnav, 0, $myDomain);
 	echo $close_nav;
-} elseif($dropdownnav=="0"){
+} else {
 	echo $open_nav;
-	writeNavigation($mainnav, "0", "");
-	echo $close_nav;
-} elseif($dropdownnav=="1"){
-	echo $open_nav;
-	writeNavigation($mainnav, "1", "");
+	writeNavigation($mainnav, $dropdownnav);
 	echo $close_nav;
 }
 
--- sitemap.php
+++ sitemap.php
@@ -5,13 +5,13 @@ include("include.config.php");
 $my_root = "C:\\users\\".$server_folder_name;
 
 // create an array of files that are not to be included
-$bad_files = array('.htaccess', '[your type]_featured.php', '404b.php', 'article.php', 'article_featured.php', 'article_featured.php.older', 'article_list.php', 'article_output.php', 'article_recent.php', 'ar_output.php', 'ar_o_list.php', 'ar_o_list_xml.php', 'body.htm', 'details.php', 'details.php.backup', 'email.php', 'emailfriend.php', 'emailfriend.php(1)', 'emailfriend.php.backup', 'email_rm.php', 'email_rm.php(1)', 'email_rm.php.backup', 'featured.include.php', 'featured.include.realestate.fade.php', 'featured.land.include.php', 'featured.rental.include.php', 'footer.htm', 'footer_print.htm', 'functions.php', 'header.htm', 'header_print.htm', 'js', 'key_f.php', 'ok.php', 'output.php', 'readme.md', 'result.php', 'rss_list.php', 'search.php', 'sitemap.php', 'sitemap.inc.php', 'test.php');
+$bad_files = array('.htaccess', '[your type]_featured.php', '404b.php', 'article.php', 'article_featured.php', 'article_featured.php.older', 'article_list.php', 'article_output.php', 'article_recent.php', 'ar_output.php', 'ar_o_list.php', 'ar_o_list_xml.php', 'body.htm', 'details.php', 'details.php.backup', 'email.php', 'emailfriend.php', 'emailfriend.php(1)', 'emailfriend.php.backup', 'email_rm.php', 'email_rm.php(1)', 'email_rm.php.backup', 'featured.include.php', 'featured.include.realestate.fade.php', 'featured.land.include.php', 'featured.rental.include.php', 'footer.htm', 'footer_print.htm', 'functions.php', 'header.htm', 'header_print.htm', 'js', 'key_f.php', 'ok.php', 'output.php', 'result.php', 'rss_list.php', 'search.php', 'sitemap.php', 'sitemap.inc.php', 'test.php');
 
 // create an array of folders that are not to be included
 $bad_folders = array('.', '..', '.git', '404b', 'css', 'images', 'lib', 'manager', 'templates');
 
 // create an array of extensions that are not to be included
-$bad_extensions = array('bmp', 'css', 'jpg', 'jpeg', 'gif', 'ini', 'png'); 
+$bad_extensions = array('bmp', 'css', '.gif', 'jpg', 'jpeg', 'ini', '.md', 'png'); 
 
 // catch any prefixes (ie include.file.php)
 $bad_prefixes = array('include'); 
