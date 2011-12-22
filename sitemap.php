d24f2e13c0477e47b9bd2ad7119d3db42ea0b809
Sean Blankenship <sean.blankenship@gmail.com>
2011-12-22 00:35:24 -0500

    refactoring

--- sitemap.php
+++ sitemap.php
@@ -4,27 +4,27 @@
 include("include.config.php");
 $my_root = "C:\\users\\".$server_folder_name;
 
+// create an array of files that are not to be included
+$bad_files = array('.htaccess', '[your type]_featured.php', '404b.php', 'article.php', 'article_featured.php', 'article_featured.php.older', 'article_list.php', 'article_output.php', 'article_recent.php', 'ar_output.php', 'ar_o_list.php', 'ar_o_list_xml.php', 'body.htm', 'details.php', 'details.php.backup', 'email.php', 'emailfriend.php', 'emailfriend.php(1)', 'emailfriend.php.backup', 'email_rm.php', 'email_rm.php(1)', 'email_rm.php.backup', 'featured.include.php', 'featured.include.realestate.fade.php', 'featured.land.include.php', 'featured.rental.include.php', 'footer.htm', 'footer_print.htm', 'functions.php', 'header.htm', 'header_print.htm', 'js', 'key_f.php', 'ok.php', 'output.php', 'readme.md', 'result.php', 'rss_list.php', 'search.php', 'sitemap.php', 'sitemap.inc.php', 'test.php');
+
+// create an array of folders that are not to be included
+$bad_folders = array('.', '..', '.git', '404b', 'css', 'images', 'lib', 'manager', 'templates');
+
+// create an array of extensions that are not to be included
+$bad_extensions = array('bmp', 'css', 'jpg', 'jpeg', 'gif', 'ini', 'png'); 
+
+// catch any prefixes (ie include.file.php)
+$bad_prefixes = array('include'); 
+
 // read the directory and spit out all files/directories
 if ($handle = opendir($my_root)) {
 
     // loop through the directory
     while (false !== ($entry = readdir($handle))) {
-
-        // create an array of files that are not to be included
-        $bad_files = array('.htaccess', '[your type]_featured.php', '404b.php', 'article.php', 'article_featured.php', 'article_featured.php.older', 'article_list.php', 'article_output.php', 'article_recent.php', 'ar_output.php', 'ar_o_list.php', 'ar_o_list_xml.php', 'body.htm', 'details.php', 'details.php.backup', 'email.php', 'emailfriend.php', 'emailfriend.php(1)', 'emailfriend.php.backup', 'email_rm.php', 'email_rm.php(1)', 'email_rm.php.backup', 'featured.include.php', 'featured.include.realestate.fade.php', 'featured.land.include.php', 'featured.rental.include.php', 'footer.htm', 'footer_print.htm', 'functions.php', 'header.htm', 'header_print.htm', 'js', 'key_f.php', 'ok.php', 'output.php', 'readme.md', 'result.php', 'rss_list.php', 'search.php', 'sitemap.php', 'sitemap.inc.php', 'test.php');
-
-        // create an array of folders that are not to be included
-        $bad_folders = array('.', '..', '.git', '404b', 'css', 'images', 'lib', 'manager', 'templates');
-
-        // create an array of extensions that are not to be included
-        $bad_extensions = array('bmp', 'css', 'jpg', 'jpeg', 'gif', 'ini', 'png');
-
+    
         // get the file extension
         $entry_explode = explode('.', $entry);
         $entry_explode = strtolower($entry_explode[1]);
-
-        // catch any prefixes (ie include.file.php)
-        $bad_prefixes = array('include');
 
         // get the file prefix
         $prefix_explode = explode('.', $entry);
@@ -45,4 +45,3 @@ if (file_exists("sitemaps.inc.php")){
 }
 
 ?>
-
