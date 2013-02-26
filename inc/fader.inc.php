bd5de0c9b70daac2505654a568f00589c797398a
Sean Blankenship <sean.blankenship@gmail.com>
2013-02-26 08:42:01 -0500

    added support for images folder

--- inc/fader.inc.php
+++ inc/fader.inc.php
@@ -1,11 +1,11 @@
 <?php
 
 if ($pageName == "default") { 
-	echo write_imgs("fader","1",".jpg");
+	echo write_imgs("images/fader","1",".jpg");
 } else {
-	if (file_exists("fader1-sub.jpg")) {       // <-- if fader1-sub.jpg exists, display it
-    	echo random_img("fader","1","-sub.jpg");
-    } elseif(file_exists("fader1.jpg")) {       // <-- else display fader1.jpg if it exists
-    	echo random_img("fader","1",".jpg");
+	if (file_exists("images/fader1-sub.jpg")) {       // <-- if fader1-sub.jpg exists, display it
+    	echo random_img("images/fader","1","-sub.jpg");
+    } elseif(file_exists("images/fader1.jpg")) {       // <-- else display fader1.jpg if it exists
+    	echo random_img("images/fader","1",".jpg");
     }
 }
