d7842317c1687e28e77341f93b248e8d5d03c9e1
Sean Blankenship <sean.blankenship@gmail.com>
2013-02-27 08:44:20 -0500

    adding standard content

--- inc/aside.inc.php
+++ inc/aside.inc.php
@@ -1,6 +1,22 @@
 <aside>
     <section>
-        <h3>Section Title</h3>
-        <p>Some text</p>
-    </section>    
+        <h3>How to Find Us</h3>
+        <?=google_map(260,300)?>
+    </section> 
+    <section>
+        <h3>Contact Information</h3>
+        <ul class="ci">
+            <li><strong>Address:</strong><?php
+                echo (!empty($myCompany)) ? '<br>'.$myCompany : '';
+                echo (!empty($myAddressOne)) ? '<br>'.$myAddressOne : '';
+                echo (!empty($myAddressTwo)) ? '<br>'.$myAddressTwo : '';
+                echo (!empty($myCity)) ? '<br>'.$myCity.', '.$myState.' '.$myZip : ''; ?></li>
+            <li><strong>Phone:</strong><?php
+                echo (!empty($myPhoneLocal)) ? '<br>Ph: '.$myPhoneLocal : '';
+                echo (!empty($myPhoneTollFree)) ? '<br>Ph: '.$myPhoneTollFree : '';
+                echo (!empty($myFax)) ? '<br>Fax: '.$myFax : ''; ?></li>
+            <li class="full"><strong>Email:</strong><br><?php 
+                echo (!empty($myEmail)) ? email_link($myEmail) : ''; ?></li>
+        </ul>
+    </section>     
 </aside>
